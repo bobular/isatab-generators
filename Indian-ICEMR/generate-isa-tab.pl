2370d31ff0e7788c3782df056f24d5f477c596a0
Sarah Kelly <s.kelly@imperial.ac.uk>
2016-12-16 14:09:07 +0000

    changed the script to add zero samples and made them pools not individuals, need to work on bloodmeal analysis

--- Indian-ICEMR/generate-isa-tab.pl
+++ Indian-ICEMR/generate-isa-tab.pl
@@ -99,7 +99,7 @@ foreach my $filename (glob "$indir/*.{txt,tsv}") {
 #
 #
 
-my @s_samples = ( ['Source Name', 'Sample Name', 'Material Type', 'Term Source Ref', 'Term Accession Number', 'Characteristics [sex (EFO:0000695)]', 'Term Source Ref', 'Term Accession Number', 'Characteristics [developmental stage (EFO:0000399)]', 'Term Source Ref', 'Term Accession Number', 'Characteristics [age (EFO:0000246)]', 'Unit', 'Term Source Ref', 'Term Accession Number' ] );
+my @s_samples = ( ['Source Name', 'Sample Name', 'Description','Material Type', 'Term Source Ref', 'Term Accession Number', 'Characteristics [sex (EFO:0000695)]', 'Term Source Ref', 'Term Accession Number', 'Characteristics [developmental stage (EFO:0000399)]', 'Term Source Ref', 'Term Accession Number', 'Characteristics [age (EFO:0000246)]', 'Unit', 'Term Source Ref', 'Term Accession Number', 'Characteristics [sample size (VBcv:0000983)]' ] );
 
 my @a_species = ( [ 'Sample Name', 'Assay Name', 'Description', 'Protocol REF', 'Characteristics [species assay result (VBcv:0000961)]', 'Term Source Ref', 'Term Accession Number' ] );
 
@@ -162,11 +162,22 @@ foreach my $row (@combined_input_rows) {
     }
   }
 
+  # create collection assay name
+  #To make my assay names for collection
+  my $a_collection_assay_name = $collection_name{$row->{Village}}{$row->{Month}}{$lat_decimal}{$long_decimal} //= sprintf "%s_%s_C%03d", $row->{Village}, $row->{Month}, ++$collection_counter;
+
+  # a_species Protocol REF
+  my $species_protocol_ref = $row->{Species} =~ /culicifacies/ ? 'SPECIES_SIB_CUL' : 'SPECIES_SIB_FLUV';
+
   # check that total moz = sum of bloodmeals
   my $total_mossies = $row->{'Total number of mosquitoes'};
 
   my @culicifacies_bm_headings = qw/BCE:BM:Bovine	BCE:BM:Human	BCE:BM:B+H BCE:BM:Unfed	BCE:BM:Others AD:BM:Bovine AD:BM:Human AD:BM:B+H AD:BM:Unfed AD:BM:Others/;
   my @fluviatilis_bm_headings = qw/T:BM:Bovine	T:BM:Human	T:BM:B+H	T:BM:Unfed	T:BM:Others	S:BM:Bovine	S:BM:Human	S:BM:B+H	S:BM:Unfed	S:BM:Others/;
+
+  # sibling species count headings
+  my @culicifacies_ss_headings = qw/SS:BCE SS:AD/;
+  my @fluviatilis_ss_headings = qw/SS:S SS:T/;
 
   # sanity check for mosquito numbers
   my $sum_bloodmeal = 0;
@@ -178,6 +189,13 @@ foreach my $row (@combined_input_rows) {
     next; # go straight to next row, do not collect...
   }
 
+  #
+  # TO DO: sanity check that SS counts add up to Total number of ...
+  #
+  warn "to do SS sanity check\n";
+
+
+
   # now we loop through each blood meal type and create an entry in the sample sheet
   # each mossie needs a Sample Name
 
@@ -186,32 +204,49 @@ foreach my $row (@combined_input_rows) {
     my ($subspecies, $always_bm, $bm_type) = split /:/, $bm_heading;
 
     my $num_mossies = $row->{$bm_heading};
-    if ($num_mossies) {
-      for (1 .. $num_mossies) {
-	# MAIN LOOP PER MOSQUITO #
-	my $sample_name = sprintf "%s %s %04d", $row->{Species}, $subspecies, ++$sample_number{$row->{Species}}{$subspecies};
-	# replace non alphanumeric with underscore
-	$sample_name =~ s/\W+/_/g;
-	
-	#for sib species
-	my $species_protocol_ref = $row->{Species} =~ /culicifacies/ ? 'SPECIES_SIB_CUL' : 'SPECIES_SIB_FLUV';
-	#To make option for sib species
-	my $protocol_ref = $row->{'Type of Dwelling'} =~ /HD/ ? 'COLL_HOUSE' : 'COLL_CATTLE';
+    if ($num_mossies && $num_mossies>0) {
+      my $sample_name = sprintf "%s %s %04d", $row->{Species}, $subspecies, ++$sample_number{$row->{Species}}{$subspecies};
+      # replace non alphanumeric with underscore
+      $sample_name =~ s/\W+/_/g;
 
-	#To make my assay names for collection
-	my $a_collection_assay_name = $collection_name{$row->{Village}}{$row->{Month}}{$lat_decimal}{$long_decimal} //= sprintf "%s_%s_C%03d", $row->{Village}, $row->{Month}, ++$collection_counter;
+      #push a reference to an array of row data into s_samples
 
-	#push a reference to an array of row data into s_samples
-
-	push @s_samples, [ '2016-indian-icemr', $sample_name, 'individual', 'EFO', '0000542', 'female', 'PATO', '0000383', 'adult', 'IDOMAL', '0000655' ];
-	push @a_species, [ $sample_name, '$sample_name.SPECIES', '', 'SPECIES', morpho_species_term($row->{Species}) ];
-	push @a_species, [ $sample_name, "$sample_name.$species_protocol_ref" , '', $species_protocol_ref, pcr_species_term($subspecies) ];
-	push @a_collection, [ $sample_name, $a_collection_assay_name, '', protocol_ref($row->{'Type of Dwelling'}), '', $row->{Month}, 'India', 'GAZ', '00002839', $lat_decimal, $long_decimal, 'IA', $row->{Village}, 'India' ]
-
-#	print STDOUT "created '$sample_name' from $row->{Village} that dined on $bm_type\n";
-      }
+      push @s_samples, [ '2016-indian-icemr', $sample_name, '',  'pool', 'EFO', '0000663', 'female', 'PATO', '0000383', 'adult', 'IDOMAL', '0000655', $num_mossies ];
+      push @a_species, [ $sample_name, "$sample_name.SPECIES", '', 'SPECIES', morpho_species_term($row->{Species}) ];
+      push @a_species, [ $sample_name, "$sample_name.$species_protocol_ref" , '', $species_protocol_ref, pcr_species_term($subspecies) ];
+      push @a_collection, [ $sample_name, $a_collection_assay_name, '', collection_protocol_ref($row->{'Type of Dwelling'}), '', $row->{Month}, 'India', 'GAZ', '00002839', $lat_decimal, $long_decimal, 'IA', $row->{Village}, 'India' ];
+      push @a_blood_species, [ $sample_name, "$sample_name.PCR_BLOOD", 'PCR_BLOOD', '', $row->{'Total number of mosquitoes'}, 'p_blood_species.txt' ];
+      push @p_blood_species, [ "$sample_name.PCR_BLOOD", "Human Bloodmeal Index: TO DO", 'identification of source of blood meal in arthropod', 'VSMO', '0000174',];
+      #	print STDOUT "created '$sample_name' from $row->{Village} that dined on $bm_type\n";
     }
 
+  }
+
+
+  # add s_samples row with sample size=0 rows when no sib species observed
+  # and 2x a_species and 1x a_collection rows too
+  my @ss_headings = $row->{Species} =~ /culic/ ? @culicifacies_ss_headings : @fluviatilis_ss_headings;
+
+  foreach my $ss_heading (@ss_headings) {
+
+    #hints:  you know species $row->{Species}
+    #        get the subspecies from a split on $ss_heading
+
+    # get the subspecies code from the heading
+    my ($tmp_ss, $subspecies) = split /:/, $ss_heading;
+
+    # if the cell is empty we assume it's a zero count for that subspecies
+    if (!$row->{$ss_heading}) {
+
+      # create Sample Name
+      my $sample_name = sprintf "%s %s %04d", $row->{Species}, $subspecies, ++$sample_number{$row->{Species}}{$subspecies};
+
+      # push rows onto s_samples, a_species and a_collection
+      push @s_samples, [ '2016-indian-icemr', $sample_name, 'Zero sample warning TO DO', 'pool', 'EFO', '0000663', 'female', 'PATO', '0000383', 'adult', 'IDOMAL', '0000655', '0' ];
+      push @a_species, [ $sample_name, "$sample_name.SPECIES", '', 'SPECIES', morpho_species_term($row->{Species}) ];
+      push @a_species, [ $sample_name, "$sample_name.$species_protocol_ref" , '', $species_protocol_ref, pcr_species_term($subspecies) ];
+      push @a_collection, [ $sample_name, $a_collection_assay_name, '', collection_protocol_ref($row->{'Type of Dwelling'}), '', $row->{Month}, 'India', 'GAZ', '00002839', $lat_decimal, $long_decimal, 'IA', $row->{Village}, 'India' ];
+    }
   }
 
   #sanity checking output
@@ -336,7 +371,7 @@ sub pcr_species_term {
   }
 }
 
-sub protocol_ref {
+sub collection_protocol_ref {
   my $input = shift;
   given ($input) {
     when (/^HD$/) {
